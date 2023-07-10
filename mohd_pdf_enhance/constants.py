70da87210348ebae90a3dd0ec76a7790e50e4544
Fabio Castelli <fabio.c@mohd.it>
2023-07-10 11:03:59 +0200

    Added version number

---
+++ mohd_pdf_enhance/constants.py
@@ -0,0 +1,9 @@
+##
+#     Project: MOHD PDF Enhance
+# Description: Apply various fixes to PDF files before processing them
+#      Author: MOHD <it@mohd.it>
+#   Copyright: 2023 MOHD
+##
+
+APP_NAME = 'mohd_pdf_enhance'
+APP_VERSION = '0.1.0'
